faf4ba052edff1c9facd96a7b83737db0f2e5377
shanujose45 <shanustudy44@gmail.com>
2026-04-27 11:00:01 +1200

    Initialize index.js for DailyHealthWebsite app

---
+++ src/src/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DailyHealthWebsite from './DailyHealthWebsite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DailyHealthWebsite />
+  </React.StrictMode>
+);
